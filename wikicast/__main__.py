bf34f8ec0b09d509722af42e7f3329669918392d
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-11-17 14:42:31 -0800

    Add script for running experiments by sampling

--- wikicast/__main__.py
+++ wikicast/__main__.py
@@ -1,6 +1,7 @@
 import click
 
 from .baseline import main as baseline
+from .baseline_random import main as baseline_random
 from .poisson import main as poisson
 from .subgraph.cli import subgraph
 
@@ -13,6 +14,7 @@ def cli():
 cli.add_command(subgraph)
 cli.add_command(poisson, "poisson")
 cli.add_command(baseline, "baseline")
+cli.add_command(baseline_random, "baseline-random")
 
 
 if __name__ == "__main__":
