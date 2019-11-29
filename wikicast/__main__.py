80ec4c7d61cb4689c5f90c51943dfec7d19e53e2
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-11-28 20:29:31 -0800

    Add new command to partition the graph once (to avoid arrow issues)

--- wikicast/__main__.py
+++ wikicast/__main__.py
@@ -14,7 +14,8 @@ cli.add_command(poisson.main, "poisson")
 cli.add_command(baseline.main, "baseline")
 cli.add_command(baseline_random.main, "baseline-random")
 cli.add_command(pipeline.main, "pipeline")
-cli.add_command(bipartition.main, "bipartition")
+cli.add_command(bipartition.partition_recursive, "bipartition")
+cli.add_command(bipartition.partition_once, "bipartition-once")
 
 
 if __name__ == "__main__":
