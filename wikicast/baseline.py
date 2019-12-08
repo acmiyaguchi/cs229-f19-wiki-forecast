bcfb53cfec432dfb86e913d27ffdbc23c75e3c18
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-12-07 18:07:43 -0800

    Update poisson to use statsmodel

--- wikicast/baseline.py
+++ wikicast/baseline.py
@@ -9,12 +9,9 @@ from sklearn import linear_model, metrics
 from sklearn.neighbors import NearestNeighbors
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
-import statsmodels.api as sm
-
 
 from .poisson import PoissonRegression
 from .data import rmse, mape, laplacian_embedding, create_dataset
-import pdb
 
 
 def plot_top(y, y_pred):
@@ -29,7 +26,12 @@ def plot_top(y, y_pred):
 
 def summarize(model_name, y, y_pred, trial_id=1):
     """Return a dictionary of results"""
-    return {"mape": mape(y, y_pred), "rmse": rmse(y, y_pred), "name": model_name, "trial_id": trial_id}
+    return {
+        "mape": mape(y, y_pred),
+        "rmse": rmse(y, y_pred),
+        "name": model_name,
+        "trial_id": trial_id,
+    }
 
 
 def plot_scree(g, k=64):
@@ -42,40 +44,59 @@ def plot_scree(g, k=64):
 def run_ablation(name, model, train, validate, test, pagerank, emb, **kwargs):
     results = []
     test_X = np.hstack([train[:, 7:], validate])
-    trial_id=kwargs["trial_id"]
+    trial_id = kwargs["trial_id"]
     del kwargs["trial_id"]
 
     z = np.hstack([train, pagerank, emb])
     model.fit(z, validate, **kwargs)
     z = np.hstack([test_X, pagerank, emb])
     results.append(
-        summarize(f"{name}: history + pagerank + emb", test, model.predict(z), trial_id=trial_id)
+        summarize(
+            f"{name}: history + pagerank + emb",
+            test,
+            model.predict(z),
+            trial_id=trial_id,
+        )
     )
 
     z = np.hstack([train, pagerank])
     model.fit(z, validate, **kwargs)
     z = np.hstack([test_X, pagerank])
-    results.append(summarize(f"{name}: history + pagerank", test, model.predict(z), trial_id=trial_id))
+    results.append(
+        summarize(
+            f"{name}: history + pagerank", test, model.predict(z), trial_id=trial_id
+        )
+    )
 
     z = np.hstack([train, emb])
     model.fit(train, validate, **kwargs)
     z = np.hstack([test_X, emb])
-    results.append(summarize(f"{name}: history + emb", test, model.predict(test_X), trial_id=trial_id))
+    results.append(
+        summarize(
+            f"{name}: history + emb", test, model.predict(test_X), trial_id=trial_id
+        )
+    )
 
     z = np.hstack([train[:, -7:], pagerank, emb])
     model.fit(z, validate, **kwargs)
     z = np.hstack([test_X[:, -7:], pagerank, emb])
-    results.append(summarize(f"{name}: pagerank + emb", test, model.predict(z), trial_id=trial_id))
+    results.append(
+        summarize(f"{name}: pagerank + emb", test, model.predict(z), trial_id=trial_id)
+    )
 
     z = np.hstack([train])
     model.fit(train, validate, **kwargs)
     z = np.hstack([test_X])
-    results.append(summarize(f"{name}: history", test, model.predict(test_X), trial_id=trial_id))
+    results.append(
+        summarize(f"{name}: history", test, model.predict(test_X), trial_id=trial_id)
+    )
 
     z = np.hstack([train[:, -7:], pagerank])
     model.fit(z, validate, **kwargs)
     z = np.hstack([test_X[:, -7:], pagerank])
-    results.append(summarize(f"{name}: pagerank", test, model.predict(z), trial_id=trial_id))
+    results.append(
+        summarize(f"{name}: pagerank", test, model.predict(z), trial_id=trial_id)
+    )
 
     z = np.hstack([train[:, -7:], emb])
     model.fit(z, validate, **kwargs)
@@ -83,7 +104,11 @@ def run_ablation(name, model, train, validate, test, pagerank, emb, **kwargs):
     results.append(summarize(f"{name}: emb", test, model.predict(z), trial_id))
 
     model.fit(train[:, -7:], validate, **kwargs)
-    results.append(summarize(f"{name}: baseline", test, model.predict(test_X[:, -7:]), trial_id=trial_id))
+    results.append(
+        summarize(
+            f"{name}: baseline", test, model.predict(test_X[:, -7:]), trial_id=trial_id
+        )
+    )
     return results
 
 
@@ -104,7 +129,7 @@ def weighted_linear_regression(train, validate, test, pagerank, emb, trial_id=1)
                 f"weighted linear regression ({name}): pagerank + emb",
                 test,
                 model.predict(z),
-                trial_id=trial_id
+                trial_id=trial_id,
             )
         )
 
@@ -113,8 +138,10 @@ def weighted_linear_regression(train, validate, test, pagerank, emb, trial_id=1)
         z = np.hstack([test_X, pagerank])
         results.append(
             summarize(
-                f"weighted linear regression ({name}): pagerank", test, model.predict(z),
-                trial_id=trial_id
+                f"weighted linear regression ({name}): pagerank",
+                test,
+                model.predict(z),
+                trial_id=trial_id,
             )
         )
 
@@ -124,8 +151,10 @@ def weighted_linear_regression(train, validate, test, pagerank, emb, trial_id=1)
         z = np.hstack([test_X, emb])
         results.append(
             summarize(
-                f"weighted linear regression ({name}): emb", test, model.predict(z),
-                trial_id=trial_id
+                f"weighted linear regression ({name}): emb",
+                test,
+                model.predict(z),
+                trial_id=trial_id,
             )
         )
 
@@ -135,7 +164,7 @@ def weighted_linear_regression(train, validate, test, pagerank, emb, trial_id=1)
                 f"weighted linear regression ({name}): baseline",
                 test,
                 model.predict(test_X),
-                trial_id=trial_id
+                trial_id=trial_id,
             )
         )
         return results
@@ -151,49 +180,6 @@ def weighted_linear_regression(train, validate, test, pagerank, emb, trial_id=1)
     emb_results = run("avg emb", weights, trial_id=trial_id)
     return pr_results + emb_results
 
-
-def poisson_regression(train, validate, test, pagerank, emb, trial_id=1):
-    results = []
-    # Poisson model with the embedding as feature
-    num_weeks = int(train.shape[1] / 7)
-    eps = 1e-20
-    weekly_train = np.zeros((train.shape[0], num_weeks))
-    for i in range(num_weeks):
-        weekly_train[:, i] = np.sum(train[:, 7*i:7*i+6], axis=1) + eps
-    weekly_validate = np.sum(validate, axis=1).reshape(-1, 1) + eps
-    weekly_test = np.sum(test, axis=1).reshape(-1, 1) + eps
-
-    test_X = np.hstack([weekly_train[:, -11:], weekly_validate])
-   
-    model = sm.GLM(endog=weekly_validate, exog=weekly_train[:, -12:], family=sm.families.Poisson())
-    fitted_model = model.fit()
-        
-    results.append(summarize("poisson regression", weekly_test, fitted_model.predict(test_X), trial_id=trial_id))
-
-    # Poisson model with pagerank + embedding as feature
-    extra_dims = pagerank.shape[1] + emb.shape[1]
-    z = np.hstack([weekly_train, pagerank+eps, emb+eps])
-    model = sm.GLM(endog=weekly_validate, exog=z[:, -12-extra_dims:], family=sm.families.Poisson())
-    fitted_model = model.fit()
-    
-    z = np.hstack([test_X, pagerank+eps, emb+eps])
-    results.append(
-        summarize("poisson regression pagerank + emb", weekly_test, fitted_model.predict(z), trial_id=trial_id)
-    )
-    return results
-
-def normalized_linear_regression(ts, window_size, num_windows, **kwargs):
-    results = []
-    trial_id = kwargs["trial_id"]
-    del kwargs["trial_id"]
-    train, validate, test = create_dataset(ts, window_size, num_windows,
-        missing_value_default=0, normalize=True)
-    model = linear_model.LinearRegression()
-
-    test_X = np.hstack([train[:, 7:], validate])
-    model.fit(train, validate, **kwargs)
-    results.append(summarize(f"linear regresson, history only, normalized data", test, model.predict(test_X), trial_id=trial_id))
-    return results
 
 def run_trial(mapping, edges, ts, plot_scree=False, trial_id=1):
     embedding_size = 8
@@ -222,20 +208,42 @@ def run_trial(mapping, edges, ts, plot_scree=False, trial_id=1):
 
     results = [
         summarize("persistence", test, validate, trial_id=trial_id),
-        summarize("mean", test, (np.ones(test.shape).T * validate.mean(axis=1)).T, trial_id=trial_id),
+        summarize(
+            "mean",
+            test,
+            (np.ones(test.shape).T * validate.mean(axis=1)).T,
+            trial_id=trial_id,
+        ),
     ]
 
     model = linear_model.LinearRegression()
     results += run_ablation(
-        "linear regression", model, train, validate, test, pagerank, emb, trial_id=trial_id
+        "linear regression",
+        model,
+        train,
+        validate,
+        test,
+        pagerank,
+        emb,
+        trial_id=trial_id,
     )
-    
-    
+
+    # run poisson,
+    poisson = PoissonRegression()
+    poisson.fit(train.mean(axis=1, keepdims=True), validate)
+    results += [
+        summarize(
+            "poisson",
+            test,
+            poisson.predict(
+                np.hstack([train[:, 7:], validate]).mean(axis=1, keepdims=True)
+            ),
+            trial_id=trial_id,
+        )
+    ]
+
     # custom ablation
     weighted_linear_regression(train, validate, test, pagerank, emb, trial_id=trial_id)
-    results += poisson_regression(train, validate, test, pagerank, emb, trial_id=trial_id)
-    
-    results += normalized_linear_regression(ts, window_size, num_windows, trial_id=trial_id)
 
     model = DecisionTreeRegressor()
     results += run_ablation(
@@ -257,7 +265,7 @@ def main(mapping_path, edges_path, ts_path):
     edges = pd.read_csv(edges_path)
     ts = pd.read_csv(ts_path)
     results = run_trial(mapping, edges, ts)
-    print(pd.DataFrame(results)[["name", "mape", "rmse", "trial_id"]])
+    print(pd.DataFrame(results)["trial_id", "name", "mape", "rmse"])
 
 
 if __name__ == "__main__":
--- wikicast/poisson.py
+++ wikicast/poisson.py
@@ -2,97 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import click
+import statsmodels.api as sm
 
 from . import data
 
 
 class PoissonRegression:
-    """Poisson Regression.
-
-    Example usage:
-        > clf = PoissonRegression(step_size=lr)
-        > clf.fit(x_train, y_train)
-        > clf.predict(x_eval)
-    """
-
-    def __init__(
-        self, step_size=1e-5, max_iter=10000000, eps=1e-5, theta_0=None, verbose=False
-    ):
-        """
-        Args:
-            step_size: Step size for iterative solvers only.
-            max_iter: Maximum number of iterations for the solver.
-            eps: Threshold for determining convergence.
-            theta_0: Initial guess for theta. If None, use the zero vector.
-            verbose: Print loss values during training.
-        """
-        self.theta = theta_0
-        self.step_size = step_size
-        self.max_iter = max_iter
-        self.eps = eps
-        self.verbose = verbose
+    def __init__(self):
+        self.model = []
+        self.fitted = []
 
     def fit(self, x, y):
-        """Run gradient ascent to maximize likelihood for Poisson regression.
-
-        Args:
-            x: Training example inputs. Shape (n_examples, dim).
-            y: Training example labels. Shape (n_examples, output_dim), or
-                (n_examples,) if output_dim = 1.
-        """
-        # *** START CODE HERE ***
-        n_examples = x.shape[0]
-        d = x.shape[1]
         if len(y.shape) == 1:
-            output_dim = 1
-        else:
-            output_dim = y.shape[1]
-
-        self.theta = np.zeros((output_dim, d))
-
-        for k in range(output_dim):
-            if output_dim == 1:
-                y_1 = y.reshape((n_examples, 1))
-            else:
-                y_1 = y[:, k].reshape((n_examples, 1))
-            previous_delta_norm = 10000 * self.eps
-            delta_norm = 1000 * self.eps
-            iter = 0
-
-            step_size = self.step_size
-            while (
-                iter < self.max_iter
-                and (np.abs(delta_norm - previous_delta_norm) / previous_delta_norm)
-                > self.eps
-            ):
-                z = np.exp(np.matmul(x, self.theta[k].T)).reshape((n_examples, 1))
-                update = np.sum((y_1 - z) * x, axis=0)
-                previous_delta_norm = delta_norm
-                delta_norm = np.linalg.norm(self.step_size * update, ord=1)
-                self.theta[k] = self.theta[k] + self.step_size * update
-                iter += 1
+            y = y.reshape(-1, 1)
+        self.model = []
+        for j in range(y.shape[1]):
+            model = sm.GLM(
+                endog=y[:, j], exog=x, family=sm.families.Poisson(), link="log"
+            )
+            self.fitted.append(model.fit())
+            self.model.append(model)
 
     def predict(self, x):
-        """Make a prediction given inputs x.
-
-        Args:
-            x: Inputs of shape (n_examples, dim).
-
-        Returns:
-            Floating-point prediction for each input, shape (n_examples, output_dim).
-        """
-        predict = np.exp(np.matmul(x, self.theta.T))
-        return predict
-
-
-def plot(eval, predict):
-    # TODO: fix this for the multi-dim case.
-    plt.scatter(y_eval, y_predict)
-    plt.axis(xmin=0, xmax=30, ymin=0, ymax=30)
-    plt.xlim(-2, y_eval.max() + 2)
-    plt.ylim(0, y_predict.max() + 2)
-    plt.xlabel("Actual")
-    plt.ylabel("Predicted")
+        results = []
+        for model, fitted in zip(self.model, self.fitted):
+            results.append(model.predict(fitted.params, x))
+        return np.array(results).T
 
 
 @click.command()
