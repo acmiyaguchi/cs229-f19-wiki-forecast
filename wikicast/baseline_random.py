21b1a60eb9550c14f81010ff2c7a4cae25ad52df
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-12-07 19:10:49 -0800

    Move plot_summary into the main project

--- wikicast/baseline_random.py
+++ wikicast/baseline_random.py
@@ -74,8 +74,8 @@ def main(pages_path, pagelinks_path, pageviews_path, num_trials, output_summary_
         start = time.time()
         mapping, edges, ts = create_dataset_from_parquet(pages, pagelinks, pageviews)
         print(f"sampling took {time.time() - start} seconds")
-
         results += run_trial(mapping, edges, ts, trial_id=trial_id)
+
     df = pd.DataFrame(results)[["name", "mape", "rmse", "trial_id"]]
     print(df)
     df.to_csv(output_summary_file)
