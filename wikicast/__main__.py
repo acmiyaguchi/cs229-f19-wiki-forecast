858079bc362300e650b8b4a9cde9bb63c78db601
shaonc <55311820+shaonc@users.noreply.github.com>
2019-12-08 14:59:59 -0800

    fix imports in wikicast module main
    fix imports in wikicast module main

--- wikicast/__main__.py
+++ wikicast/__main__.py
@@ -1,22 +1,21 @@
 import click
 
-from . import baseline, baseline_random, poisson, pipeline, bipartition
-from .subgraph.cli import subgraph
-
+from . import baseline, baseline_random, poisson, pipeline, bipartition, experiment_embedding
+#from .subgraph.cli import subgraph
 
 @click.group()
 def cli():
     pass
 
 
-cli.add_command(subgraph)
+#cli.add_command(subgraph)
 cli.add_command(poisson.main, "poisson")
 cli.add_command(baseline.main, "baseline")
 cli.add_command(baseline_random.main, "baseline-random")
 cli.add_command(pipeline.main, "pipeline")
 cli.add_command(bipartition.partition_recursive, "bipartition")
 cli.add_command(bipartition.partition_once, "bipartition-once")
-
+cli.add_command(experiment_embedding.main, "experiment-embedding")
 
 if __name__ == "__main__":
     cli()
